008bca951fbba8009323e2a02c17a99891fb9f2f
Markel Mencía <markel.mnc@gmail.com>
2026-01-04 15:25:30 +0100

    Fix sitemap generation

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -2,7 +2,6 @@ import fs from 'fs'
 import matter from 'gray-matter'
 
 function generateSitemap() {
-  
   const postFiles = fs.readdirSync('posts')
   const posts = postFiles.map((filename) => {
     const slug = filename.replace('.md', '')
@@ -23,26 +22,24 @@ function generateSitemap() {
   
   const staticPages = [
     { url: '', changefreq: 'weekly', priority: '1.0' },
-    { url: 'blog', changefreq: 'weekly', priority: '0.9' },
-    { url: 'writeups', changefreq: 'weekly', priority: '0.9' },
-    { url: 'projects', changefreq: 'weekly', priority: '0.8' },
+    { url: '/blog', changefreq: 'weekly', priority: '0.9' },
+    { url: '/writeups', changefreq: 'weekly', priority: '0.9' },
+    { url: '/projects', changefreq: 'weekly', priority: '0.8' },
   ]
   
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${staticPages.map(page => `  <url>
-    <loc>https://markelmencia.com/${page.url}</loc>
+    <loc>https://markelmencia.com${page.url}</loc>
     <changefreq>${page.changefreq}</changefreq>
     <priority>${page.priority}</priority>
-  </url>
-  `)}
+  </url>`).join('\n')}
 ${allPosts.map(post => `  <url>
     <loc>https://markelmencia.com/${post.type}/${post.slug}</loc>
     <lastmod>${post.date}</lastmod>
     <changefreq>yearly</changefreq>
     <priority>0.7</priority>
-  </url>
-  `)}
+  </url>`).join('\n')}
 </urlset>`
   
   fs.writeFileSync('./public/sitemap.xml', sitemap)
